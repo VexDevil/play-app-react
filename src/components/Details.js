c808bece3bbd2ec372b4e08c8f644f2c7c42144e
Ravi Singh <Ravisinghrajputana9@gmail.com>
2022-07-15 14:37:18 +0530

    Initial commit

---
+++ src/components/Details.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ function Details() {
+  return 
+    <div className = "details">
+        
+        <div className='details_img'>
+            <img src = "" alt = ""/>
+
+        </div>
+        <h3 className='details_title'>Song Title</h3>
+        <h3 className='details_artist'>Song artist</h3>
+        
+      
+    </div>;
+  
+}
